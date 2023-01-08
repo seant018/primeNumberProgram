153d1f7294db95916add472e8fbfae050f5971c1
Sean Thompson <120729543+seant018@users.noreply.github.com>
2023-01-08 15:50:47 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import math, sys
+
+
+def main():
+    # requires a file named "data.txt", which must have a number only
+    f = open("data.txt")
+    num = int(f.read())
+    #input("press Ctrl-C to quit. Press Enter to begin.")
+    f.close()
+
+    while True:
+        if isPrime(num):
+            with open("data.txt", "w") as f:
+                f.write(str(num))
+            print(str(num) + ", \n", end="", flush=True)
+        num = num + 1
+
+
+def isPrime(number):
+    # Return True if the number is prime, otherwise False
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
